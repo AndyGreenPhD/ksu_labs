d0c68bf67f2e313307f82c321e0916c3da862b91
Andy Green <agreen57@kennesaw.edu>
2023-09-25 23:53:00 -0400

    remove keys

--- 3210_labs/module/_init.tf
+++ 3210_labs/module/_init.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = "AKIAY5QSTR7LMCDVIOKY"
-  secret_key = "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"
+  access_key = "ACCESS_KEY VALUE GOES HERE"
+  secret_key = "SECRET_KEY VALUE GOES HERE"
 }
